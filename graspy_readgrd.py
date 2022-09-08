a45caffd12b332e2d5b3c71c52ef1481a7b25791
albers <roland.albers@unibe.ch>
2022-09-08 18:08:57 +0200

    can parse file

--- graspy_readgrd.py
+++ graspy_readgrd.py
@@ -3,82 +3,81 @@ import xarray as xr
 import numpy as np
 import sys
 
-f = open('farfield_54.grd','r')
+attr_sphere= [
+    {1: "linear thet and phi",2: "rhc and lhc",3: "ludwigs co and cx",4: "major and minor axes",5: "xpd E_thet/E_phi and E_phi/E_thet",6: "xpd rhc/lhc and lhc/rhc",7: "xpd co/cx and cx/co",8: "xpd major/minor and minor/major",9: "total power and sqrt rhc/lhc"},
+    {2: "two field comp",3: "three field comp",5: "rcs for both polarisations"},
+    {1: "uv-grid",4: "elevation over azimuth",5: "elevation and azimuth",6: "azimuth over elevation",7: "theta_phi grid", 9: "azimuth over elevation,edx",10: "elevation over azimuth,edx"}
+    ]
 
-header=[]
-while 1:
-    line = f.readline()
-    if line[0:4] == "++++":
-        break
-    else:
-        header.append(line)
+def readgrid(fname: str,attr_key=attr_sphere):
+    f_grid = open(fname,'r')
 
-freq=[]
-for i in header[5].split():
-    freq.append(float(i))
+    header=[]
+    while 1:
+        line = f_grid.readline()
+        if line[0:4] == "++++":
+            break
+        else:
+            header.append(line)
 
-ktype=f.readline()
-nset,icomp,ncomp,igrid=[int(s) for s in f.readline().split()]
+    freq=[]
+    for i in header[5].split():
+        freq.append(float(i))
 
-beamc=[]
-for i in range(int(nset)):
-    beamc.append([float(s) for s in f.readline().split()])
+    ktype=f_grid.readline()
+    nset,icomp,ncomp,igrid=[int(s) for s in f_grid.readline().split()]
 
-lims=f.readline().split()
-xlims=[float(s) for s in lims[0:3:2]]
-ylims=[float(s) for s in lims[1:4:2]]
+    beamc=[]
+    for i in range(int(nset)):
+        beamc.append([float(s) for s in f_grid.readline().split()])
 
-nx,ny,klimit=[int(s) for s in f.readline().split()]
+    list_ds=[]
+    for idx,f in enumerate(freq):
+        lims=f_grid.readline().split()
+        xlims=[float(s) for s in lims[0:3:2]]
+        ylims=[float(s) for s in lims[1:4:2]]
 
-stpx=(xlims[1]-xlims[0])/(nx-1)
-stpy=(ylims[1]-ylims[0])/(ny-1)
+        nx,ny,klimit=[int(s) for s in f_grid.readline().split()]
 
-if klimit==1:
-    sys.exit("In .grd file KLIMIT = 1. Code not finished for this")
-else:
-    Is=1
-    Ie=nx
+        stpx=(xlims[1]-xlims[0])/(nx-1)
+        stpy=(ylims[1]-ylims[0])/(ny-1)
 
-matrix=np.zeros(shape=(nx,ny,2),dtype=complex)
-for x in range(nx):
+        matrix=np.zeros(shape=(nx,ny,4),dtype=float)
 
-    for y in range(ny):
-        line=f.readline().split()
-        matrix[x,y,0]=complex(float(line[0]), float(line[1]))
-        matrix[x,y,1]=complex(float(line[2]), float(line[3]))
-matrix4d = np.expand_dims(matrix, 3)
-ds = xr.Dataset(
-    data_vars=dict(
-        Field=(["xcor","ycor","fco","f"], matrix4d)
-    ),
-    coords=dict(
-        xcor=(["xcor"],np.linspace(xlims[0],xlims[1],nx)),
-        ycor=(["ycor"],np.linspace(ylims[0],ylims[1],ny)),
-        fco=(["fco"],["co","cross"]),
-        f=(["f"],[50]),
-    ),
-    attrs=dict(description="What am I doing?"),
-)
-ds['fco'].attrs['first element'] = 'co'
-ds['fco'].attrs['second element'] = 'cross'
-
-ds2 = xr.Dataset(
-    data_vars=dict(
-        Field=(["xcor","ycor","fco","f"], matrix4d)
-    ),
-    coords=dict(
-        xcor=(["xcor"],np.linspace(xlims[0],xlims[1],nx)),
-        ycor=(["ycor"],np.linspace(ylims[0],ylims[1],ny)),
-        fco=(["fco"],["co","cross"]),
-        f=(["f"],[51]),
-    ),
-    attrs=dict(description="What am I doing?"),
-)
-ds['fco'].attrs['first element'] = 'co'
-ds['fco'].attrs['second element'] = 'cross'
-
-list_ds = [ds, ds2]
-
-#list_ds.append(ds2)
-
-xr.merge(list_ds)
+        if klimit==1:
+            sys.exit("In .grd file KLIMIT = 1. Code not finished for this")
+        else:
+            Is=1
+            Ie=nx
+        for x in range(nx):
+            for y in range(ny):
+                line=f_grid.readline().split()
+                matrix[x,y,0]=float(line[0])
+                matrix[x,y,1]=float(line[1])
+                matrix[x,y,2]=float(line[2])
+                matrix[x,y,3]=float(line[3])
+        matrix4d = np.expand_dims(matrix, 3)
+        ds = xr.DataArray(
+            data=matrix4d,
+            dims=["xcor","ycor","comp","band"],
+            coords=dict(
+                xcor=(["xcor"],np.linspace(xlims[0],xlims[1],nx)),
+                ycor=(["ycor"],np.linspace(ylims[0],ylims[1],ny)),
+                comp=(["comp"],["re_co","i_co","re_x","i_x"]),
+                band=(["band"],[idx+1]),
+            ),
+            attrs=dict(
+                filename=fname,
+                nset=[nset,"number of field sets or beams"],
+                icomp=[icomp,attr_sphere[0][icomp]],
+                ncomp=[ncomp,attr_sphere[1][ncomp]],
+                igrid=[igrid,attr_sphere[2][igrid]],
+                source_field=header[2][19:],
+                freq_name=header[3][16:],
+                freqs=freq
+            ),
+        )
+        list_ds.append(ds)
+    ds=xr.concat(list_ds,dim="band")
+    return ds
+# %%
