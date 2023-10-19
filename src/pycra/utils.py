0d12830a53b329ba386621e489e923d90e544e14
albers <roland.albers@unibe.ch>
2023-10-19 09:56:58 +0200

    made generic db function for normalised and non normalised and added power detection

--- src/pycra/utils.py
+++ src/pycra/utils.py
@@ -26,13 +26,42 @@ def check_grid_or_cut_type(icomp: int, ncomp: int, ival: int, ftype: str) -> [di
     return attributes, grid_type
 
 
-def decibel(array: xr.DataArray) -> xr.DataArray:
-    db_array = 20 * np.log10(np.abs(array))
+def db_norm(array: xr.DataArray) -> xr.DataArray:
+    if str(array.coords['comp'].values) == "power":
+        db_array = 10 * np.log10(np.abs(array))
+        print("power array detected")
+    else:
+        db_array = 20 * np.log10(np.abs(array))
     max_vals = []
     if np.size(db_array.freq) > 1:
         for i in db_array.freq:
             mval = db_array.sel(freq=i).max(dim=db_array.dims[0:2])
             db_array.loc[{'freq': i}] = db_array.sel(freq=i) - mval.max()
+            # mval is also a DataArray, so we need extract the values which are a numpy array and convert them to a
+            # list.
+            max_vals.append(mval.values.tolist())
+    else:
+        max_vals = db_array.max(dim=db_array.dims[0:2])
+        db_array = db_array - max_vals.max()
+        max_vals = max_vals.values.tolist()
+    db_array.name = f'{array.name}_dB'
+    db_array.attrs["units"] = "dB"
+    db_array.attrs["long_name"] = "Normalised directivity"
+    db_array.attrs["peak_gain"] = max_vals
+    db_array = db_array.assign_attrs(array.attrs)
+    return db_array
+
+
+def decibel(array: xr.DataArray) -> xr.DataArray:
+    if str(array.coords['comp'].values) == "power":
+        db_array = 10 * np.log10(np.abs(array))
+        print("power array detected")
+    else:
+        db_array = 20 * np.log10(np.abs(array))
+    max_vals = []
+    if np.size(db_array.freq) > 1:
+        for i in db_array.freq:
+            mval = db_array.sel(freq=i).max(dim=db_array.dims[0:2])
             # mval is also a DataArray, so we need extract the values which are a numpy array and convert them to a
             # list.
             max_vals.append(mval.values.tolist())
